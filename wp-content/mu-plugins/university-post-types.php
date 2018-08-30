89f20ef2cd3f9154bc1adc11d8744ca554b53691
AhHyunSK <ahhyunsim@gmail.com>
2018-08-31 03:36:35 +0900

    Updated all

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,8 +1,11 @@
-<?php 
+<?php
+
 function university_post_types() {
   // Campus Post type
-  register_post_type('campus',array(
-    'supports' => array('title','editor','excerpt'),
+  register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'campuses'),
     'has_archive' => true,
     'public' => true,
@@ -16,10 +19,11 @@ function university_post_types() {
     'menu_icon' => 'dashicons-location-alt'
   ));
 
-
   // Event Post type
-  register_post_type('event',array(
-    'supports' => array('title','editor','excerpt'),
+  register_post_type('event', array(
+    'capability_type' => 'event',
+    'map_meta_cap' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
@@ -33,44 +37,71 @@ function university_post_types() {
     'menu_icon' => 'dashicons-calendar'
   ));
 
+  // Program Post Type
+  register_post_type('program', array(
+    'supports' => array('title'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
 
-//program Post Type
-register_post_type('program',array(
-  'supports' => array('title','editor',),
-  'rewrite' => array('slug' => 'programs'),
-  'has_archive' => true,
-  'public' => true,
-  'labels' => array(
-    'name' => 'Programs',
-    'add_new_item' => 'Add New Program',
-    'edit_item' => 'Edit Program',
-    'all_items' => 'All Programs',
-    'singular_name' => 'Program'
-  ),
-  'menu_icon' => 'dashicons-awards'
-));
 
+  // Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
 
-//professor Post Type
-register_post_type('professor',array(
-  'show_in_rest' => true,
-  'supports' => array('title','editor','thumbnail'), 
-  'public' => true,
-  'labels' => array(
-    'name' => 'Professors',
-    'add_new_item' => 'Add New Professor',
-    'edit_item' => 'Edit Professor',
-    'all_items' => 'All Professors',
-    'singular_name' => 'Professor'
-  ),
-  'menu_icon' => 'dashicons-welcome-learn-more'
-));
+  // Note Post Type
+  register_post_type('note', array(
+    'capability_type' => 'note',
+    'map_meta_cap' => true,
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'public' => false,
+    'show_ui' => true,
+    'labels' => array(
+      'name' => 'Notes',
+      'add_new_item' => 'Add New Note',
+      'edit_item' => 'Edit Note',
+      'all_items' => 'All Notes',
+      'singular_name' => 'Note'
+    ),
+    'menu_icon' => 'dashicons-welcome-write-blog'
+  ));
 
+  // Like Post Type
+  register_post_type('like', array(
+    'supports' => array('title'),
+    'public' => false,
+    'show_ui' => true,
+    'labels' => array(
+      'name' => 'Likes',
+      'add_new_item' => 'Add New Like',
+      'edit_item' => 'Edit Like',
+      'all_items' => 'All Likes',
+      'singular_name' => 'Like'
+    ),
+    'menu_icon' => 'dashicons-heart'
+  ));
 
 }
 
-
-
-add_action('init','university_post_types');
-
-?>
+add_action('init', 'university_post_types');
